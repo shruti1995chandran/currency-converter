0c5f8d96e6f900e5af8151563faca844cfb4b911
shruti1995chandran <shrutichandran95@gmail.com>
2022-01-31 22:06:46 +0000

    First batch - server code 2

--- server/dist/lib/src/utility/customMessage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SUCCESS_CASE = exports.ERROR_CASE = void 0;
-exports.ERROR_CASE = {
-    UNAUTHORIZED_USER: "User is not authorized to continue!",
-    INVALID_REQUEST: "Request is invalid!",
-};
-exports.SUCCESS_CASE = {};
-//# sourceMappingURL=customMessage.js.map
